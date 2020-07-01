62b4bb62c2afe82000464e5a1def384e8a0c2513
Zach Choate <zach.a.choate@gmail.com>
2020-07-01 11:22:33 -0400

    Add files via upload

---
+++ New-OptionList.ps1
@@ -0,0 +1,56 @@
+
+function New-OptionList {
+
+    <#
+
+    .DESCRIPTION
+        Use to create a Powershell option list and return the selected value.
+    .PARAMETER title
+        Title of the option list menu you'd like to specifiy.
+    .PARAMETER optionsList
+        Comma separated list of values to build your option menu from.
+    .PARAMETER message
+        Message to prepend before the option values and on the post selection page.
+    .EXAMPLE
+        $searchBy = New-OptionList -Title "Search for user by" -optionsList "displayname", "email", "userprincipalname" -message "to search by"
+            This will create a menu with the options specified and return the $selectedValue to the $searchBy variable for use in the script.
+    .NOTES
+        Adapted from script on https://www.business.com/articles/powershell-interactive-menu/
+
+        Version:        1.0
+        Updated:        07/01/2020
+        Created:        07/01/2020
+        Author:         Zach Choate
+
+    #>
+
+    param (
+           [string]$title,
+           [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+           [string[]]$optionsList=@(),
+           [string]$message
+    )
+
+    Clear-Host
+    Write-Host "================ $title ================"
+    
+    $n = 1
+    ForEach($option in $optionsList) {
+
+        Write-Host "$n`: Press `'$n`' $message $option."
+        $n++
+
+    }
+    Write-Host "Q: Press 'Q' to quit."
+    $optionSelected = Read-Host "Please make a selection"
+    If($optionSelected -eq "q") {
+        Write-Host "Quiting..."
+        Exit
+    }
+    $optionSelected = $optionSelected-1
+    $selectedValue = $optionsList[$optionSelected]
+
+    Write-Host "You selected $message $selectedValue. `nMoving on..."
+    Return $selectedValue
+
+}
